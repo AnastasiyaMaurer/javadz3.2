aba623581cdcd6b6abad0876e93f0eac0477dbe6
Anastasiya <namirono@mail.ru>
2023-10-06 11:28:14 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weightInKg = 98;
+        double heightInMeters = 1.87;
+        double bmi = service.calculate (weightInKg, heightInMeters);
+        System.out.println("Имт: " + bmi);
+    }
+}
